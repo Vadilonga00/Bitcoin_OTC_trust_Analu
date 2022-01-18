1148fb0542bafb421b0ba9d75a646009b6a8363a
Vadilonga00 <francyvadi@gmail.com>
2022-01-18 17:02:45 +0100

    aggiunto file main dove vengono richiamate le varie funzioni

---
+++ main.m
@@ -0,0 +1,7 @@
+[A,G,n] = readgraphs(bitcoin);
+
+deg_in = indegree(G); %mi restituisce per ogni nodo una colonna con il numero di connessioni che puntano verso l'interno in corrispondenza di un vertice
+
+deg_out = outdegree(G) % mi restituisce per ogni nodo una colonna con il numero di connessioni che hanno origine in un vertice e puntano verso l'esterno verso altri vertici
+
+plot(G)
