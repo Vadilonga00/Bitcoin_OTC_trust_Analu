a64d7cae61536234ea3be566d588f5907dfefa4e
Vadilonga00 <francyvadi@gmail.com>
2022-01-19 18:28:21 +0100

    aggiunto richiamo alla funzione PWC

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 [A,G,n] = readgraphs(bitcoin);
 
-deg_in = indegree(G); %mi restituisce per ogni nodo una colonna con il numero di connessioni che puntano verso l'interno in corrispondenza di un vertice
+[deg_in,deg_out] = Degree(G)
 
-deg_out = outdegree(G) % mi restituisce per ogni nodo una colonna con il numero di connessioni che hanno origine in un vertice e puntano verso l'esterno verso altri vertici
+[importanza_PWC] = PWC(A,n);
 
 plot(G)
